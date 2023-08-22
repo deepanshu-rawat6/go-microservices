7c322b499247842541a469cb3eb1bc96fedce82a
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-08-22 16:39:01 +0530

    updated: product-api/handlers/hello.go

--- product-api/handlers/hello.go
+++ product-api/handlers/hello.go
@@ -7,21 +7,31 @@ import (
 	"net/http"
 )
 
+// Hello -> simple handler
 type Hello struct {
 	l *log.Logger
 }
 
+// Creates a new hello handler with the given logger
 func NewHello(l *log.Logger) *Hello {
 	return &Hello{l}
 }
 
+// ServeHTTP implements the go http.Hanlder interface
+//
+//	https://pkg.go.dev/net/http#Handler
 func (h *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("Hello, World!")
+	h.l.Println("Hello, World!")
+
+	// read the body
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		h.l.Println("Error reading body", err)
+
 		http.Error(w, "Oops", http.StatusBadRequest)
 		return
 	}
 
+	// Writing response
 	fmt.Fprintf(w, "Hello, %s", d)
 }
